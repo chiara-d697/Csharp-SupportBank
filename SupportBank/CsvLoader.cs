9b5eaf64848abd0a846c1f35c2d545ea2d015239
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-02-26 18:01:48 +0000

    csv loader now is reading line by line

--- SupportBank/CsvLoader.cs
+++ SupportBank/CsvLoader.cs
@@ -3,10 +3,25 @@ namespace SupportBank
     public class CsvLoader : IFileLoader
     {
         public string[] FileLoader(String fileName)
-        {
-           string[] lines = File.ReadAllLines(fileName);
+        // {
+        //    string[] lines = File.ReadAllLines(fileName);
 
-           return lines;
+        //    return lines;
+        // }
+        {
+           string[] lines = new string[] {};
+           using (StreamReader sr = new StreamReader(fileName))
+            {
+                // Discard header row
+                sr.ReadLine();            
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines = lines.Concat(new string[] { line }).ToArray();
+                }
+            }
+            return lines;
+
         }
 
     }
